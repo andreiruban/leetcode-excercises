eb76364356cb705af4066cad2b192d45e3754e5d
andreiruban <herr.ruban@yandex.com>
2021-01-23 00:13:52 +0100

    ListNode.kt has been introduced

---
+++ src/main/kotlin/recursion/ListNode.kt
@@ -0,0 +1,25 @@
+package recursion
+
+class ListNode(var `val`: Int, var next: ListNode?) {
+
+    override fun toString(): String {
+        val printed = "$`val`"
+        val cursorNode = next
+        return printed + printNode(cursorNode)
+    }
+
+    private fun printNode(cursorNode: ListNode?): String = when {
+        cursorNode != null -> {
+            val result = "->${cursorNode.`val`}"
+            result + printNode(cursorNode.next)
+        }
+        else -> {
+            "->null"
+        }
+    }
+}
+
+fun main() {
+    val given = ListNode(`val` = 1, ListNode(`val` = 2, ListNode(`val` = 3, next = null)))
+    println(given)
+}
