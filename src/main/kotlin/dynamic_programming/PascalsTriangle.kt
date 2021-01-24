e33fb03bb4fcd50a341817509e2ff360fe8e2eec
andreiruban <herr.ruban@yandex.com>
2021-01-25 00:02:46 +0100

    PascalsTriangle.kt has been introduced

---
+++ src/main/kotlin/dynamic_programming/PascalsTriangle.kt
@@ -0,0 +1,25 @@
+package dynamic_programming
+
+import dynamic_programming.PascalsTriangle.generate
+
+
+object PascalsTriangle {
+    fun generate(numRows: Int): List<List<Int>> {
+
+        if (numRows == 0) return emptyList()
+
+        val triangle = mutableListOf(listOf(1))
+
+        (1 until numRows).forEach { rowI ->
+            val prev = triangle.last()
+            val next = listOf(1) + (1 until rowI).map { rowJ -> prev[rowJ] + prev[rowJ - 1] }.toList() + listOf(1)
+            triangle.add(next)
+        }
+
+        return triangle
+    }
+}
+
+fun main() {
+    println(generate(5))
+}
