7b242f680a19ab6c8da7c5857ad74242fbb6939f
andreiruban <herr.ruban@yandex.com>
2021-01-26 15:16:48 +0100

    PowXN.kt recursive

---
+++ src/main/kotlin/algorithms/recursion/PowXN.kt
@@ -0,0 +1,39 @@
+package algorithms.recursion
+
+import algorithms.recursion.Pow.recursive
+
+object Pow {
+
+    /**
+     * Time: O(n)
+     * Space: O(1)
+     * */
+    fun recursive(x: Double, n: Int): Double = when {
+        n < 0 -> {
+            fastPow(1 / x, -n.toLong())
+        }
+        else -> {
+            fastPow(x, n.toLong())
+        }
+    }
+
+    private fun fastPow(x: Double, n: Long): Double {
+        if (n == 0L) {
+            return 1.0
+        }
+
+        val half = fastPow(x, n / 2)
+        return when {
+            n % 2 == 0L -> {
+                half * half
+            }
+            else -> {
+                half * half * x
+            }
+        }
+    }
+}
+
+fun main() {
+    println(recursive(2.0, 100))
+}
