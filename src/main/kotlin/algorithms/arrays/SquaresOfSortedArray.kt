6c9803958a0651601fb801044b4445e892416cb4
andreiruban <herr.ruban@yandex.com>
2021-01-29 17:14:16 +0100

    SquaresOfSortedArray.kt

---
+++ src/main/kotlin/algorithms/arrays/SquaresOfSortedArray.kt
@@ -0,0 +1,36 @@
+package algorithms.arrays
+
+import algorithms.arrays.SquaresOfSortedArray.sortedSquares
+
+object SquaresOfSortedArray {
+    fun sortedSquares(A: IntArray): IntArray? {
+        val n = A.size
+        var j = 0
+        while (j < n && A[j] < 0) j++
+        var i = j - 1
+        val ans = IntArray(n)
+        var t = 0
+        while (i >= 0 && j < n) {
+            if (A[i] * A[i] < A[j] * A[j]) {
+                ans[t++] = A[i] * A[i]
+                i--
+            } else {
+                ans[t++] = A[j] * A[j]
+                j++
+            }
+        }
+        while (i >= 0) {
+            ans[t++] = A[i] * A[i]
+            i--
+        }
+        while (j < n) {
+            ans[t++] = A[j] * A[j]
+            j++
+        }
+        return ans
+    }
+}
+
+fun main() {
+    println(sortedSquares(intArrayOf(-3, -1, 0, 1, 3, 10))?.joinToString())
+}
